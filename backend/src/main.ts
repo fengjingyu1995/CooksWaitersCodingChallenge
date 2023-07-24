d751a42428519b2b2a412bda3027327bb58ff178
Jingyu Feng <fengjingyu1995@gmail.com>
2023-07-23 20:18:35 -0400

    load data from json files to mongodb

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,26 +1,37 @@
-import Fastify, { FastifyInstance } from 'fastify';
+import Fastify from 'fastify';
 import fastifyMongo from '@fastify/mongodb';
+import cooksData from './data/cooks.json';
+import waitersData from './data/waiters1.json';
 
 const fastify = Fastify({
   logger: {
     transport: {
+      // TODO: check env to only use 'pino-pretty' in non-prod env.
+      // should not use it in production
       target: 'pino-pretty',
     },
   },
 });
 
-async function dbConnector(fastify: FastifyInstance) {
-  fastify.register(fastifyMongo, {
-    url: 'mongodb://localhost:27017',
-  });
+fastify.register(fastifyMongo, {
+  forceClose: true,
+  url: 'mongodb://localhost:27017',
+});
 
-  fastify.log.info('Connected to database');
-}
+fastify.ready(async () => {
+  // create db
+  const db = fastify.mongo.client.db('restaurant');
 
-fastify.register(dbConnector);
+  // drop the collection and then create a new collection so that we can load data from json files every time we restart the server.
+  db.dropCollection('staff');
+  const collection = db.collection('staff');
 
-fastify.get('/', async () => {
-  return { message: 'hello' };
+  // load data from json files
+  const cooksDocument = { name: 'cooks', data: cooksData };
+  const waitersDocument = { name: 'waiters', data: waitersData };
+
+  // insert docs to db
+  collection.insertMany([cooksDocument, waitersDocument]);
 });
 
 async function main() {
