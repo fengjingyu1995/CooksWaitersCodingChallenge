de2e5a62087c6d390f4823e42331325beaee73cd
Jingyu Feng <fengjingyu1995@gmail.com>
2023-07-25 16:28:55 -0400

    Be able to show waiters data

--- frontend/src/components/Nav/index.jsx
+++ frontend/src/components/Nav/index.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
-    <nav className='w-full h-16 bg-gray-300'>
+    <nav className='w-full h-16 bg-blue-400 '>
       <ul className='flex flex-row w-full'>
         <li className='flex items-center justify-center'>
           <Link to='/Waiters' className='h-full p-5 text-center hover:bg-white'>
--- frontend/src/pages/Waiters/index.jsx
+++ frontend/src/pages/Waiters/index.jsx
@@ -1,5 +1,53 @@
+import { useState, useMemo } from 'react';
+
+const DAYS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
+
+const DATA = {
+  monday: ['Howard', 'Martin', 'Michael', 'Bert'],
+  tuesday: ['Roy', 'Herbert', 'Jacob', 'Tom', 'Elmer', 'Carl', 'Lee'],
+  wednesday: ['Peter', 'Benjamin', 'Frederick', 'Willie', 'Alfred', 'Sam'],
+  thursday: ['Will', 'Jesse', 'Oscar', 'Lewis'],
+  friday: [
+    'Herman',
+    'Jim',
+    'Francis',
+    'Harvey',
+    'Earl',
+    'Eugene',
+    'Ralph',
+    'Ed',
+  ],
+};
+
 const Waiters = () => {
-  return <div>Waiters</div>;
+  // TODO: fetch data from server
+  const [data, setData] = useState(DATA);
+  // TODO: should be saved in app level using context
+  const [currentDayIdx, setCurrentDayIdx] = useState(0);
+
+  const currentDay = DAYS[currentDayIdx];
+  const waiters = useMemo(() => {
+    return data[DAYS[currentDayIdx]];
+  }, [data, currentDayIdx]);
+
+  return (
+    <>
+      <h1 className='p-5 text-2xl text-center'>Waiters</h1>
+
+      <h2>
+        Current Day: <span className='capitalize'>{currentDay}</span>
+      </h2>
+      <ul>
+        {waiters.map((waiter) => (
+          // Assuming waiter's name is unique
+          <li key={waiter}>{waiter}</li>
+        ))}
+      </ul>
+
+      <button className='p-3 m-2 bg-blue-400 '>prev</button>
+      <button className='p-3 m-2 bg-blue-400 '>next</button>
+    </>
+  );
 };
 
 export default Waiters;
