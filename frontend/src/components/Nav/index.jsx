caf4d67b41494e35bd94957eae2c0f81d8163f1e
Jingyu Feng <fengjingyu1995@gmail.com>
2023-07-26 14:19:00 -0400

    Update navigation button color on hover and selection

--- frontend/src/components/Nav/index.jsx
+++ frontend/src/components/Nav/index.jsx
@@ -1,16 +1,27 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Nav = () => {
+  const { pathname } = useLocation();
   return (
     <nav className='w-full h-16 bg-blue-600 '>
       <ul className='flex flex-row w-full'>
         <li className='flex items-center justify-center'>
-          <Link to='/Waiters' className='h-full p-5 text-center hover:bg-white'>
+          <Link
+            to='/Waiters'
+            className={`h-full p-5 text-center hover:bg-white ${
+              pathname === '/Waiters' ? 'bg-white' : ''
+            }`}
+          >
             Waiters
           </Link>
         </li>
         <li className='flex items-center justify-center'>
-          <Link to='/Cooks' className='h-full p-5 text-center hover:bg-white '>
+          <Link
+            to='/Cooks'
+            className={`h-full p-5 text-center hover:bg-white ${
+              pathname === '/Cooks' ? 'bg-white' : ''
+            }`}
+          >
             Cooks
           </Link>
         </li>
